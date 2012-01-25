a490b20b90040a690df685982fe4e503d6f1e9ee
Bharath Padmanabhan <b476p030@ku.edu>
2012-01-24 22:12:02 -0600

    initial

---
+++ HtmlPrettyPrinter/junit/edu/ku/hpp/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package edu.ku.hpp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
